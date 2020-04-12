743686166dc4e685aa3488ae394a898a5c33cb5c
Nicolas Ezequiel Puccio <nico.puccio@hotmail.com>
2020-04-12 16:37:29 -0300

    Implemented a function to properly get two random pokemons and show them in the screen. All data is gathered from de pokemon API. Added a button to refresh pokemons. Any pokemon can appear up to Gen 3.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //getPokemon(30);
+let list = document.querySelector("#pokemonList");
+let refreshBtn = document.querySelector("#refresh-Button");
 
-
-function getPokemon(id, num){
+function getPokemon(id, num)
+{
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     .then((response) => response.json())
-    .then((pokemon) => console.log(pokemon.name));
+    .then((pokemon) => createPokemon(pokemon, num));
 }
 
 function getRandomPokemons()
@@ -15,4 +17,16 @@ function getRandomPokemons()
     getPokemon(firstId, 1);
     getPokemon(secondId,2);
 }
+
+function createPokemon(pokemon, num) 
+{
+    //convert data to HTML
+    let item = list.querySelector(`#poke-${num}`);
+    let img = item.getElementsByTagName("img")[0];
+    img.setAttribute("src", pokemon.sprites.front_default);
+    let name = item.getElementsByTagName("p")[0];
+    name.textContent = pokemon.name;
+}
 getRandomPokemons();
+
+refreshBtn.addEventListener("click", (e) => getRandomPokemons());
